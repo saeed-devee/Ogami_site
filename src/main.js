048e7ce582db946d4a18f04db6ca781b656c3c8a
saeed-devee <saeedshokrollahi90@gmail.com>
2025-09-01 18:33:12 +0330

    slider is all sets !

--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
-import './style.css'
+import './style.css';
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 
-const swiperSlider = new Swiper();
+const swiperSlider = new Swiper('.swiper',{
+    direction: 'horizontal',
+    loop: true,
+    // autoplay:{
+    //     delay: 4000
+    // },
+
+    effect: 'fade',
+    fadeEffect: {
+    crossFade: true,
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
+
+const swiper = document.querySelector('.swiper').swiper;
 
